6cbb217cc64ce808f49aaba39253a6e1fec68827
Angelo Pirola <angelo9871@gmail.com>
2023-08-07 00:01:21 +0200

    Aggiunta classe confirm

---
+++ src/CustomLibrary.ProblemDetails/Response/Confirm.cs
@@ -0,0 +1,34 @@
+﻿namespace CustomLibrary.ProblemDetails.Response;
+
+public class Confirm
+{
+    public HttpStatusCode StatusCode { get; }
+    public bool Success { get; set; }
+    public object Message { get; set; }
+
+    public Confirm()
+    {
+        StatusCode = HttpStatusCode.OK;
+        Success = true;
+    }
+
+    public Confirm(object message)
+    {
+        StatusCode = HttpStatusCode.OK;
+        Success = true;
+        Message = message;
+    }
+
+    public Confirm(HttpStatusCode statusCode, bool success)
+    {
+        StatusCode = statusCode;
+        Success = success;
+    }
+
+    public Confirm(HttpStatusCode statusCode, bool success, object message)
+    {
+        StatusCode = statusCode;
+        Success = success;
+        Message = message;
+    }
+}
