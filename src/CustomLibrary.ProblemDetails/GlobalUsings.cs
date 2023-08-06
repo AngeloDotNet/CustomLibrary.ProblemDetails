b6de4b8703106feea9d9d3097fcde0d7b5204ad7
Angelo Pirola <angelo9871@gmail.com>
2023-08-07 00:01:50 +0200

    Fix

--- src/CustomLibrary.ProblemDetails/GlobalUsings.cs
+++ src/CustomLibrary.ProblemDetails/GlobalUsings.cs
@@ -1,1 +1,5 @@
-﻿
+﻿global using System.Diagnostics;
+global using System.Net;
+global using CustomLibrary.ProblemDetails.Common;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
