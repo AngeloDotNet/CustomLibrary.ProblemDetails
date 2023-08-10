075838dd70a7c93b5ad9bd54ad0538070f7858d3
Angelo Pirola <angelo9871@gmail.com>
2023-08-11 00:09:55 +0200

    Implementata exception status code 401

---
+++ src/CustomLibrary.ProblemDetails/Exception/UnauthorizedException.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomLibrary.ProblemDetails.Exception;
+
+public class UnauthorizedException : System.Exception
+{
+    public UnauthorizedException()
+    {
+    }
+
+    public UnauthorizedException(string message) : base(message)
+    {
+    }
+
+    public UnauthorizedException(string message, System.Exception innerException) : base(message, innerException)
+    {
+    }
+}
--- src/CustomLibrary.ProblemDetails/Response/Response.cs
+++ src/CustomLibrary.ProblemDetails/Response/Response.cs
@@ -45,4 +45,26 @@ public static class Response
 
         return result;
     }
+
+    public static ObjectResult Unauthorized(HttpContext httpContext, System.Exception exc)
+    {
+        var statusCode = StatusCodes.Status401Unauthorized;
+        var problemDetails = new CustomProblemDetails
+        {
+            Status = statusCode,
+            Type = $"https://httpstatuses.com/{statusCode}",
+            Instance = httpContext.Request.Path,
+            Title = "Unauthorized"
+        };
+
+        problemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? httpContext.TraceIdentifier);
+        problemDetails.Extensions.Add("errors", exc.Message);
+
+        var result = new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode
+        };
+
+        return result;
+    }
 }
