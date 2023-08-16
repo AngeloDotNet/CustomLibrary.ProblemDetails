1e6c3e0c0cb0a5ccc79cd7c30e9033b44bf3f0a5
Angelo Pirola <angelo9871@gmail.com>
2023-08-17 00:03:48 +0200

    Implementata exception status code 404 closes #13

--- src/CustomLibrary.ProblemDetails/Response/Response.cs
+++ src/CustomLibrary.ProblemDetails/Response/Response.cs
@@ -89,4 +89,26 @@ public static class Response
 
         return result;
     }
+
+    public static ObjectResult NotFound(HttpContext httpContext, System.Exception exc)
+    {
+        var statusCode = StatusCodes.Status404NotFound;
+        var problemDetails = new CustomProblemDetails
+        {
+            Status = statusCode,
+            Type = $"https://httpstatuses.com/{statusCode}",
+            Instance = httpContext.Request.Path,
+            Title = "NotFound"
+        };
+
+        problemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? httpContext.TraceIdentifier);
+        problemDetails.Extensions.Add("errors", exc.Message);
+
+        var result = new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode
+        };
+
+        return result;
+    }
 }
