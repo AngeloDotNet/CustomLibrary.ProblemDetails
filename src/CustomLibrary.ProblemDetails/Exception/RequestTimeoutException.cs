7d51b766f732a20346809a20f5b42949b7bca27d
Angelo Pirola <angelo9871@gmail.com>
2023-08-27 15:50:28 +0200

    Implementata exception status code 408 close #15

---
+++ src/CustomLibrary.ProblemDetails/Exception/RequestTimeoutException.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomLibrary.ProblemDetails.Exception;
+
+public class RequestTimeoutException : System.Exception
+{
+    public RequestTimeoutException()
+    {
+    }
+
+    public RequestTimeoutException(string message) : base(message)
+    {
+    }
+
+    public RequestTimeoutException(string message, System.Exception innerException) : base(message, innerException)
+    {
+    }
+}
