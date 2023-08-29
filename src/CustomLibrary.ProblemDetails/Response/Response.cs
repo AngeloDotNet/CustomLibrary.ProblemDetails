90d7a6dc8cb05090ccd1b1db0010c4b1de9f024c
Angelo Pirola <angelo9871@gmail.com>
2023-08-29 08:51:05 +0200

    Implementata exception status code 409

--- src/CustomLibrary.ProblemDetails/Response/Response.cs
+++ src/CustomLibrary.ProblemDetails/Response/Response.cs
@@ -177,4 +177,26 @@ public static class Response
 
         return result;
     }
+
+    public static ObjectResult Conflict(HttpContext httpContext, System.Exception exc)
+    {
+        var statusCode = StatusCodes.Status409Conflict;
+        var problemDetails = new CustomProblemDetails
+        {
+            Status = statusCode,
+            Type = $"https://httpstatuses.com/{statusCode}",
+            Instance = httpContext.Request.Path,
+            Title = "Conflict"
+        };
+
+        problemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? httpContext.TraceIdentifier);
+        problemDetails.Extensions.Add("errors", exc.Message);
+
+        var result = new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode
+        };
+
+        return result;
+    }
 }
