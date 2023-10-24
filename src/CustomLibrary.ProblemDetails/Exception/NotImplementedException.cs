eb5fc1a8a9b64d23b8f696d9afb9da753b37cc8a
Angelo Pirola <angelo9871@gmail.com>
2023-10-24 09:51:26 +0200

    Implementata exception status code 501 - close #5

---
+++ src/CustomLibrary.ProblemDetails/Exception/NotImplementedException.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomLibrary.ProblemDetails.Exception;
+
+public class NotImplementedException : System.Exception
+{
+    public NotImplementedException()
+    {
+    }
+
+    public NotImplementedException(string message) : base(message)
+    {
+    }
+
+    public NotImplementedException(string message, System.Exception innerException) : base(message, innerException)
+    {
+    }
+}
--- src/CustomLibrary.ProblemDetails/ResponseException.cs
+++ src/CustomLibrary.ProblemDetails/ResponseException.cs
@@ -309,4 +309,32 @@ public static class ResponseException
 
         return result;
     }
+
+    public static ObjectResult NotImplemented(HttpContext httpContext, System.Exception exc, List<string> validationError = null)
+    {
+        var statusCode = StatusCodes.Status501NotImplemented;
+        var problemDetails = new CustomProblemDetails
+        {
+            Status = statusCode,
+            Detail = exc.Message,
+            Type = $"https://httpstatuses.com/{statusCode}",
+            Instance = httpContext.Request.Path,
+            Title = "InternalServerError"
+        };
+
+        problemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? httpContext.TraceIdentifier);
+        //problemDetails.Extensions.Add("errors", exc.Message);
+
+        if (validationError?.Any() ?? false)
+        {
+            problemDetails.Extensions.Add("errors", validationError);
+        }
+
+        var result = new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode
+        };
+
+        return result;
+    }
 }
